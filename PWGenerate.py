bc199c0c8707e679637e6812623d97b002f925d4
Dilan Samarasinghe <covertskippy55@gmail.com>
2014-06-04 11:29:40 -0400

    This second commit will create an alphanumeric password including special characters of given length, the length is given as a command line argument. Usage is PWGenerate.py length

--- PWGenerate.py
+++ PWGenerate.py
@@ -18,7 +18,7 @@ def generate_pw(length):
         asciitable.append(i)
     for i in range(length):
         try:
-            rand = unpack("I",urandom(4))
+            rand = unpack("I", urandom(4))
         except NotImplementedError:
             print("Your operating system does not support random number generation and this program cannot generate a "
                   "truly random password for you.")
@@ -28,14 +28,15 @@ def generate_pw(length):
         rand %= 91
         password += str(unichr(asciitable[rand]))
     return password
+
+
 def main():
     if len(sys.argv) < 2:
         print "Invalid number of arguments, usage is: PWGenerate.py length"
         sys.exit(0)
     else:
-       length =  int(sys.argv[1])
-       print generate_pw(10)
+        length = int(sys.argv[1])
+        print generate_pw(length)
 
 if __name__ == '__main__':
-  main()
-
+    main()
